0fef81fdf3e2629ef65e5c074b3967ab84214ceb
Logan Garland <lgarland@marana.com>
2019-03-13 10:44:37 -0700

    cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,18 +38,6 @@ module.exports = {
         display: 'standalone',
         icon: `src/images/icon.png`,
         include_favicon: true,
-        // icons: [
-        //   {
-        //     src: '/favicons/android-chrome-192x192.png',
-        //     sizes: '192x192',
-        //     type: 'image/png',
-        //   },
-        //   {
-        //     src: '/favicons/android-chrome-512x512.png',
-        //     sizes: '512x512',
-        //     type: 'image/png',
-        //   },
-        // ],
       },
     },
     /* Must be placed at the end */
